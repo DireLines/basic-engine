9112b6d2ffc9168aed916225e97498d1f906d9de
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2020-08-06 23:53:17 -0400

    move MatrixPool impl

--- src/engine/math/MatrixPool.h
+++ src/engine/math/MatrixPool.h
@@ -8,28 +8,8 @@ using namespace std;
 #include <iostream>
 class MatrixPool {
 public:
-    double** create() {
-        for (int i = 0; i < pool.size(); ++i) {
-            if (!inUse[i]) {
-                inUse[i] = true;
-                return pool[i];
-            }
-        }
-        double **new_mat = new double *[3];
-        for (int i = 0; i < 3; i++) {
-            new_mat[i] = new double[3];
-        }
-        pool.push_back(new_mat);
-        inUse.push_back(true);
-        return new_mat;
-    }
-    void release(double** ptr) {
-        for (int i = 0; i < pool.size(); ++i) {
-            if (pool[i] == ptr) {
-                inUse[i] = false;
-            }
-        }
-    }
+    double** create();
+    void release(double** ptr);
 private:
     vector<double**> pool;
     vector<bool> inUse;
