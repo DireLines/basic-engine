d6767986e45d52325af130280bd8e304f3768669
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2020-09-07 01:03:05 -0400

    stop mallocing on box collider support

--- src/engine/components/colliders/BoxCollider.h
+++ src/engine/components/colliders/BoxCollider.h
@@ -7,16 +7,24 @@ class BoxCollider: public Collider {
 public:
     double width = 1;
     double height = 1;
+    BoxCollider() {
+        double w = width / 2;
+        double h = height / 2;
+        corners.push_back(Vector2(w, h));
+        corners.push_back(Vector2(w, -h));
+        corners.push_back(Vector2(-w, -h));
+        corners.push_back(Vector2(-w, h));
+    }
     Vector2 support(Vector2 direction) {
         double w = width / 2;
         double h = height / 2;
-        vector<Vector2> corners {
-            Vector2(w, h),
-            Vector2(w, -h),
-            Vector2(-w, -h),
-            Vector2(-w, h),
-        };
+        corners[0] = Vector2(w, h);
+        corners[1] = Vector2(w, -h);
+        corners[2] = Vector2(-w, -h);
+        corners[3] = Vector2(-w, h);
         return Polygon::support(direction, corners);
     }
+private:
+    vector<Vector2> corners;
 };
 #endif
